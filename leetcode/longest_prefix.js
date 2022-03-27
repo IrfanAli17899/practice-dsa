0098ec14c550b4cf312e90e6ab6ca7fee522e535
irfanali17899 <irfanali17899@gmail.com>
2022-03-27 17:06:32 +0500

    longest_prefix solved.

---
+++ leetcode/longest_prefix.js
@@ -0,0 +1,19 @@
+const arr = ["flower", "flow", "flight"];
+
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function (strs) {
+  let prefix = "";
+  if (!strs.length) return prefix;
+  prefix = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    while (!strs[i].startsWith(prefix)) {
+      prefix = prefix.substring(0, prefix.length - 1);
+    }
+  }
+  return prefix;
+};
+
+console.log(longestCommonPrefix(arr)); // fl;
